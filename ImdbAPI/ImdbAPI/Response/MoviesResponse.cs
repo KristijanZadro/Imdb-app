97a6c77b3e7c7566b4ba655329517dbaf404d780
KristijanZadro <kristijan.zadro.dev@gmail.com>
2021-10-04 16:14:49 +0200

    add movie and actor responses

---
+++ ImdbAPI/ImdbAPI/Response/MoviesResponse.cs
@@ -0,0 +1,20 @@
+﻿using ImdbAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImdbAPI.Response
+{
+    public class MoviesResponse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CoverImage { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public double AverageRating { get; set; }
+        public IEnumerable<Rating> Ratings { get; set; }
+        public IEnumerable<ActorsResponse> Cast { get; set; }
+    }
+}
