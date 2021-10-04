7bed8fdb8aad443420a4089defa95d2639e7c3af
KristijanZadro <kristijan.zadro.dev@gmail.com>
2021-10-04 16:15:35 +0200

    seed data

--- ImdbAPI/ImdbAPI/Data/AppDbContext.cs
+++ ImdbAPI/ImdbAPI/Data/AppDbContext.cs
@@ -22,7 +22,8 @@ namespace ImdbAPI.Data
                    Title = "Film1",
                    CoverImage = "http://slika.jpeg",
                    Description = "ovo je desc",
-                   ReleaseDate = new DateTime(2011, 6, 10)
+                   ReleaseDate = new DateTime(2011, 6, 10),
+                   AverageRating = 8.4
                },
                new Movie
                {
@@ -30,7 +31,8 @@ namespace ImdbAPI.Data
                    Title = "Film2",
                    CoverImage = "http://slika.jpeg",
                    Description = "ovo je desc",
-                   ReleaseDate = new DateTime(2000, 3, 15)
+                   ReleaseDate = new DateTime(2000, 3, 15),
+                   AverageRating = 9.1
                }
             );
 
@@ -68,6 +70,37 @@ namespace ImdbAPI.Data
                 new MovieActor { MovieId = 2, ActorId = 1 },
                 new MovieActor { MovieId = 2, ActorId = 2 }
             );
+
+            builder.Entity<Rating>().HasData(
+               new Rating
+               {
+                   Id = 1,
+                   StarRating = 7,
+                   MovieId = 1
+               },
+               new Rating
+               {
+                   Id = 2,
+                   StarRating = 7,
+                   MovieId = 2
+               },
+               new Rating
+               {
+                   Id = 3,
+                   StarRating = 6,
+                   MovieId = 1
+               },
+               new Rating
+               {
+                   Id = 4,
+                   StarRating = 9,
+                   MovieId = 2
+               }
+            );
         }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<MovieActor> Cast { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
     }
 }
