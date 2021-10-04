c3ec6294c2f0aec50b91ee95d013968087a40a84
KristijanZadro <kristijan.zadro.dev@gmail.com>
2021-10-04 19:42:05 +0200

    header component

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <h1>IMDB</h1>
+        </div>
+    )
+}
